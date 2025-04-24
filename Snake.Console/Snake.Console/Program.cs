83460427f449c671d9887d9cd5d2f66c85860dc8
Skymile <mpatryk97@gmail.com>
2025-04-24 19:44:25 +0200

    snake: random position generator

--- Snake.Console/Snake.Console/Program.cs
+++ Snake.Console/Snake.Console/Program.cs
@@ -2,8 +2,8 @@
 int MapHeight = 15;
 int SnakeSpeed = 100;
 int SnakeCount = 6;
-int WallsCount = 6;
-int ApplesCount = 2;
+int WallsCount = 180;
+int ApplesCount = 4;
 (int X, int Y) StartingPosition = (4, 5);
 bool isAsync = true;
 Console.CursorVisible = false;
@@ -12,21 +12,38 @@ Console.CursorVisible = false;
 var snake = new LinkedList<(int X, int Y)>();
 for (int i = 0; i < SnakeCount; i++)
     snake.AddLast(StartingPosition);
-var walls = new List<(int X, int Y)>();
-for (int i = 0; i < WallsCount; i++)
+var fullSnake = new List<(int X, int Y)>() { StartingPosition };
+for (int i = 0; i < MapWidth; i++)
+    fullSnake.Add((i, StartingPosition.Y));
+
+var walls = GetRandomPositions(fullSnake, WallsCount).ToList();
+var apples = GetRandomPositions(fullSnake.Concat(walls), ApplesCount).ToList();
+
+IEnumerable<(int X, int Y)> GetRandomPositions(
+    IEnumerable<(int X, int Y)> taken, int count)
 {
-    int x = Random.Shared.Next(0, MapWidth);
-    int y = Random.Shared.Next(0, MapHeight - 1);
-    if (y >= StartingPosition.Y) ++y;
-    walls.Add((x, y));
-}
-var apples = new List<(int X, int Y)>();
-for (int i = 0; i < ApplesCount; i++)
-{
-    int x = Random.Shared.Next(0, MapWidth);
-    int y = Random.Shared.Next(0, MapHeight - 1);
-    if (y >= StartingPosition.Y) ++y;
-    apples.Add((x, y));
+    var dict = taken
+        .GroupBy(i => i.X)
+        .ToDictionary(i => i.Key, i => i.Select(i => i.Y).ToHashSet());
+
+    List<(int X, int Y)> freeCells = [];
+    for (int x = 0; x < MapWidth; x++)
+        for (int y = 0; y < MapHeight; y++)
+        {
+            if (dict.TryGetValue(x, out var takenY) &&
+                takenY.Contains(y))
+                continue;
+
+            freeCells.Add((x, y));
+        }
+
+    for (int r = 0; r < count; r++)
+    {
+        int number = Random.Shared.Next(0, freeCells.Count);
+        var found = freeCells[number];
+        freeCells.RemoveAt(number);
+        yield return found;
+    }
 }
 
 (int X, int Y) GetDirection() =>
@@ -65,15 +82,14 @@ while (true)
     for (int i = 0; i < apples.Count; i++)
     {
         var apple = apples[i];
-        if (apple.X == first.X && apple.Y == first.Y)
-        {
-            apples.RemoveAt(i);
-            snake.AddFirst(first);
+        if (apple.X != first.X || apple.Y != first.Y)
+            continue;
+        apples.RemoveAt(i);
+        snake.AddFirst(first);
 
-            int x = Random.Shared.Next(0, MapWidth);
-            int y = Random.Shared.Next(0, MapHeight);
-            apples.Add((x, y));
-        }
+        int x = Random.Shared.Next(0, MapWidth);
+        int y = Random.Shared.Next(0, MapHeight);
+        apples.Add((x, y));
     }
 
     snake.RemoveLast();
@@ -89,15 +105,15 @@ while (true)
     Console.SetCursorPosition(first.X * 2, first.Y);
     Console.Write('O');
 
-    foreach (var p in walls)
-    {
-        Console.SetCursorPosition(p.X * 2, p.Y);
-        Console.Write('#');
-    }
-
     foreach (var p in apples)
     {
         Console.SetCursorPosition(p.X * 2, p.Y);
         Console.Write('@');
+    }
+
+    foreach (var p in walls)
+    {
+        Console.SetCursorPosition(p.X * 2, p.Y);
+        Console.Write('#');
     }
 }
