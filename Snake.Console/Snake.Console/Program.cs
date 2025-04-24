9859a1ccef25065a63ef617588f3f2305a377dd0
Skymile <mpatryk97@gmail.com>
2025-04-24 18:50:26 +0200

    snake: first iteration

--- Snake.Console/Snake.Console/Program.cs
+++ Snake.Console/Snake.Console/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://github.com/Skymile/SC2025
+// https://discord.gg/JAzZZ3yS
+
+// .NET Framework 4.8
+// .NET Core -> 9.0
+//    .NET 9.0
+// .NET Standard 
+// Mono
+//
+int MapWidth = 20;
+int MapHeight = 15;
+bool isAsync = true;
+Console.CursorVisible = false;
+
+(int X, int Y) dir = (1, 0);
+var snake = new LinkedList<(int X, int Y)>();
+snake.AddLast((4, 5));
+snake.AddLast((4, 5));
+snake.AddLast((4, 5));
+snake.AddLast((4, 5));
+snake.AddLast((4, 5));
+snake.AddLast((4, 5));
+snake.AddLast((4, 5));
+
+(int X, int Y) GetDirection() =>
+    Console.ReadKey(true).Key switch
+    {
+        ConsoleKey.W => dir.Y > 0 ? dir : (+0, -1),
+        ConsoleKey.S => dir.Y < 0 ? dir : (+0, +1),
+        ConsoleKey.A => dir.X > 0 ? dir : (-1, +0),
+        ConsoleKey.D => dir.X < 0 ? dir : (+1, +0),
+        _ => dir
+    };
+
+var task = Task.Run(GetDirection);
+
+while (true)
+{
+    if (isAsync)
+        Thread.Sleep(200);
+
+    if (!isAsync || task.IsCompleted)
+    {
+        dir = await task;
+        task = Task.Run(GetDirection);
+    }
+    var last = snake.Last!.Value;
+    var first = snake.First!.Value;
+    snake.AddFirst((
+        (first.X + dir.X + MapWidth ) % MapWidth , 
+        (first.Y + dir.Y + MapHeight) % MapHeight
+    ));
+    snake.RemoveLast();
+
+    Console.SetCursorPosition(last.X * 2, last.Y);
+    Console.Write(' ');
+
+    foreach (var p in snake)
+    {
+        Console.SetCursorPosition(p.X * 2, p.Y);
+        Console.Write('*');
+    }
+}
