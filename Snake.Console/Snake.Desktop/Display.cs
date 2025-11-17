c021dcdc26d105dd405065ff4912dae6d5587805
Patryk Milewski <mpatryk97@gmail.com>
2025-11-17 19:02:08 +0100

    Desktop async version

--- Snake.Console/Snake.Desktop/Display.cs
+++ Snake.Console/Snake.Desktop/Display.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Shapes;
 
 namespace Snake.Desktop
--- Snake.Console/Snake.Desktop/MainWindow.xaml.cs
+++ Snake.Console/Snake.Desktop/MainWindow.xaml.cs
@@ -5,12 +5,6 @@ namespace Snake.Desktop
 {
     public partial class MainWindow : Window
     {
-        private const int MapWidth = 30;
-        private const int MapHeight = 20;
-        private readonly Input<Key> input;
-        private readonly Display output;
-        private readonly Board board;
-
         public MainWindow()
         {
             InitializeComponent();
@@ -18,15 +12,41 @@ namespace Snake.Desktop
             output = new Display(this, MapWidth, MapHeight);
             board = new Board();
             output.Print(board.GetSnake(), MapWidth, MapHeight, board.Apple);
+
+            if (!IsDebug)
+                Dispatcher.BeginInvoke(async () =>
+                {
+                    while (true)
+                    {
+                        await Task.Delay(TimeoutInMs);
+                        Step();
+                    }
+                });
         }
 
-        private void Window_KeyDown(object sender, KeyEventArgs e)
+        private void Step()
         {
-            var dir = input.GetDirection(() => e.Key);
             board.Move(dir);
             if (board.IsGameOver(MapWidth, MapHeight))
                 Application.Current.Shutdown();
             output.Print(board.GetSnake(), MapWidth, MapHeight, board.Apple);
         }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            dir = input.GetDirection(() => e.Key);
+            if (IsDebug)
+                Step();
+        }
+
+        private const bool IsDebug = true;
+        private const int TimeoutInMs = 100;
+        private const int MapWidth = 30;
+        private const int MapHeight = 20;
+
+        private readonly Input<Key> input;
+        private readonly Display output;
+        private readonly Board board;
+        private Direction dir = Direction.Right;
     }
 }
