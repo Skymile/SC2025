3c833c32fed47816fccc360d0cf34c80ee925c0a
Patryk Milewski <mpatryk97@gmail.com>
2025-12-15 19:05:55 +0100

    Generic binding

--- Snake.Console/Biometrics/ViewModels/MainWindowVM.cs
+++ Snake.Console/Biometrics/ViewModels/MainWindowVM.cs
@@ -1,12 +1,14 @@
-﻿using System.IO;
+﻿using System.ComponentModel;
+using System.IO;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 using Biometrics.Core;
 using Biometrics.ImageProcessing;
 
 namespace Biometrics.ViewModels;
 
-public class MainWindowVM
+public class MainWindowVM : INotifyPropertyChanged
 {
     public MainWindowVM()
     {
@@ -20,27 +22,37 @@ public class MainWindowVM
         Files = [.. Directory.EnumerateFiles("../../../Samples/")
                 .Select(Path.GetFileNameWithoutExtension)
                 .OfType<string>()];
+
+        SelectedFile = Files[0];
+        SelectedWindow = Windows[0];
     }
 
-    public IImage GetImage() => new Image(fileToPath[Files[SelectedFile]])
+    public IImage GetImage() => new Image(fileToPath[SelectedFile])
         .Apply(new Grayscale())
         .Apply(new MedianFilter())
         .Apply(new MedianFilter())
-        .Apply(new ConvolutionFilter(windowDict[Windows[SelectedWindow]])
+        .Apply(new ConvolutionFilter(windowDict[SelectedWindow])
         {
             Sensitivity = 1.0
         })
         ;
 
-    public int SelectedFile { get; set; }
-    public string[] Files { get; set; }
+    private void Set<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        field = value;
+        PropertyChanged?.Invoke(this, new(propertyName));
+    }
 
-    public int SelectedWindow { get; set; }
-    public string[] Windows { get; set; } = windowDict.Keys.ToArray();
+    public string SelectedFile   { get => field; set => Set(ref field, value); }
+    public string SelectedWindow { get => field; set => Set(ref field, value); }
+    public string[] Files        { get => field; set => Set(ref field, value); }
+    public string[] Windows      { get => field; set => Set(ref field, value); } = windowDict.Keys.ToArray();
 
     private static readonly Dictionary<string, double[]> windowDict = typeof(Window3x3)
         .GetFields(BindingFlags.Public | BindingFlags.Static)
         .ToDictionary(i => i.Name, i => (double[])i.GetValue(null)!);
 
     private readonly Dictionary<string, string> fileToPath = [];
+
+    public event PropertyChangedEventHandler? PropertyChanged;
 }
