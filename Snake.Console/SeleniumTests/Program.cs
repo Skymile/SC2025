aa2679c474206619b2040fa4b0bee48b7a30baab
Patryk Milewski <mpatryk97@gmail.com>
2026-01-19 18:28:18 +0100

    Selenium tests

---
+++ Snake.Console/SeleniumTests/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+var driver = new ChromeDriver();
+
+driver.Navigate().GoToUrl("https://www.w3schools.com/cs/index.php");
+
+Thread.Sleep(1_000);
+
+var menu = driver
+    .FindElement(By.Id("leftmenuinnerinner"));
+
+var links = menu
+    .FindElements(By.TagName("a"))
+    .Select(e => (Text: e.GetAttribute("innerHTML"), Link: e.GetAttribute("href")))
+    .ToArray();
+
+foreach (var i in links)
+    Console.WriteLine($"{i.Text}: {i.Link}");
+
+foreach (var i in links)
+{
+    driver.Navigate().GoToUrl(i.Link);
+    Thread.Sleep(500);
+}
